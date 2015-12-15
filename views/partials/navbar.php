b6844ba35c366f470ac9199332f7a953538a2778
zgulde <zachgulde@gmail.com>
2015-12-15 10:42:41 -0600

    links now go to places

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,8 +1,8 @@
 <nav> 
     <div class="container">
         <ul class="nav nav-justified nav-pills">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Browse</a></li>
+            <li><a href="/index.php">Home</a></li>
+            <li><a href="/ads.show.php">Browse</a></li>
             <li><a href="#">Login</a></li>
             <li><a href="#">Sign Up</a></li>
         </ul>
