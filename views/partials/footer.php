f8ede07322da2a33bf3d5c3eed957e8b0f24e39b
zgulde <zachgulde@gmail.com>
2015-12-15 10:41:30 -0600

    removed icons, added contact us

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,7 +1,15 @@
 <hr>
 <footer class="container">
-    <p class=>created with... <span class="pull-right"><a href="https://github.com/LZJCodeup/AdLister">LZJ</a></span></p>
-    <a class="btn btn-default btn-sm" href=""><i class="fa fa-github"></i></a>
-    <a class="btn btn-default btn-sm" href=""><i class="fa fa-html5"></i>
+    <p class="text-right footer-github">
+        LZJ
+        <a class="btn btn-default btn-xs" href="https://github.com/LZJCodeup/AdLister/">
+            <i class="fa fa-github"></i>
+        </a>
+    </p>
+    <p class="text-right">
+        <a href="mailto:zachgulde@gmail.com, lettyfuentes2013@gmail.com, jeraldsaenz@me.com">
+            Contact Us
+        </a>
+    </p>
 </a>
 </footer>
