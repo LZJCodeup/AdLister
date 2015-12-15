c839cf2d842db93913ab3c9144a0888e992c4a67
Jerald Saenz <jeraldsaenz@me.com>
2015-12-15 15:19:23 -0600

    made changes to user: edit, show, create and login to link to other pages

--- public/users.create.php
+++ public/users.create.php
@@ -3,26 +3,16 @@
 <html>
 <head>
   <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-  <title>Profile</title>
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-  <!-- Jquery -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    
-    <!-- Latest compiled and minified JavaScript -->
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
+    <?php include '../views/partials/header.php'; ?>
+    <?php include '../views/partials/navbar.php'; ?>
   <div class="container">
       <form class="form-signin">
         <h2 class="form-signin-heading">Please sign up</h2>
@@ -50,10 +40,10 @@
             <label for="inputPasswordAgain">Retype Password</label>
             <input type="password" id="password2" class="form-control" placeholder="Retype Password" required>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign Up</button>
+        <a class="btn btn-lg btn-success btn-block" href="users.show.php" type="submit">Sign Up</a>
       </form>
 
     </div> <!-- /container -->
-
+  <?php include '../views/partials/footer.php'; ?>
 </body>
 </html>
