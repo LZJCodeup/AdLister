79c904b09d5dbc692eb5ae55afe362712b50e854
Letty Fuentes <lettyfuentes2013@gmail.com>
2015-12-15 19:00:57 -0600

    Created the edit form using twitter bootstrap

--- public/ads.create.php
+++ public/ads.create.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-	    <title>Viewing All Ads</title>
+	    <title>Create Ad</title>
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
@@ -10,27 +10,30 @@
 	    <link rel="stylesheet" href="/css/main.css">
 	</head>
 	<body>
-		<h1>Create Ad</h1>
-		<form name="create-ad-form" method="POST" action="">
-			<p>
-				<label for="category-static-label" form="create-ad-form">Category</label>
+		<h1 class="text-center">Create Ad</h1>
+        <div id="ad-create-frame" class="container-fluid">
+		   <form name="create-ad-form">
+			<div class="form-group">
+				<label for="category-static-label" form="create-ad-form" class="col-sm-2 control-label">Category</label>
 				<!-- specify type -->
-				<input type="" name="category-dropdown-selector" id="category-dropdown-selector"><br>
-			</p>
-			<p>
-				<label for="posting-title-static-label" form="create-ad-form">Posting Title</label>
-				<input type="text" name="posting-title-txtbox" id="posting-title-txtbox">
+				<select name="category-dropdown-selector" class="form-control" id="category-dropdown-selector"></select>
+			</div>
+			<div class="form-group">
+				<label for="posting-title-static-label" form="create-ad-form" class="col-sm-2 control-label">Posting Title</label>
+				<input type="text" name="posting-title-txtbox" class="form-control" id="posting-title-txtbox">
 
-				<label for="price-static-label" form="create-ad-form">Price $</label>
-				<input type="text" name="category-dropdown-selector" id="category-dropdown-selector"><br>
-			</p>
-			<p>
-				<label for="posting-body-static-label" form="create-ad-form">Posting Body</label><br>
+				<label for="price-static-label" form="create-ad-form" class="col-sm-2">Price $</label>
+				<input type="text" name="price-txtbox" class="form-control" id="price-txtbox">
+			</div>
+			<div class="form-group">
+				<label for="posting-body-static-label" form="create-ad-form" class="col-sm-2">Posting Body</label><br>
 				<!-- specify type -->
-				<input type="" name="posting-body-txtbox" id="posting-body-txtbox"><br>
-			</p>	
-			<button type="submit" name="upload-img" id="upload-img" value="upload-img">Upload Image</button>
-			<button type="submit" name="submit" id="submit" value="submit">Submit</button>
-		</form>
+				<textarea class="form-control" rows="10" id="posting-body-txtbox"></textarea>
+			</div>	
+			<button type="button" name="upload-img" id="upload-img" value="upload-img" class="btn btn-default 
+					btn-lg btn-center">Upload Image</button>
+			<button type="button" name="submit" id="submit" value="submit" class="btn btn-default btn-lg btn-center">Submit</button>
+		  </form>
+      </div>
 	</body>
 </html>
