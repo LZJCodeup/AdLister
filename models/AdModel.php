d61c1af6aef7a79ed27c0871234de4fc2625fdc9
zgulde <zachgulde@gmail.com>
2015-12-16 15:49:30 -0600

    think i have the models done, just need to test them

---
+++ models/AdModel.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'BaseModel.php';
+
+class AdModel extends BaseModel {
+    protected static $table = 'ads';
+}
+
---
+++ models/CategoryModel.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'BaseModel.php';
+
+class CategoryModel extends BaseModel{
+    protected static $table = 'categories';
+}
