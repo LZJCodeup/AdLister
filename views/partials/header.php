52bb7a9aa8205e70e04a33826d0699606732462c
zgulde <zachgulde@gmail.com>
2015-12-18 11:02:41 -0600

    typo

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,8 +1,8 @@
 <header class="container form-inline">
     <h1>
-        Ad Lister!
+        <a href="/index.php" style="color:black;">Ad Lister!</a>
         <form action="/ads.index.php" class="pull-right">
-            <input type="text" class="form-control" name="query" placeholder="Search for an ad title...">
+            <input type="text" class="form-control" name="query" placeholder="Search for an ad...">
             <input type="submit" class="form-control" value="Go!">
         </form>
     </h1>
