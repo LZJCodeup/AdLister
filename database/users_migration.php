3f8c862f0cddf11927d67af7ed7589019a61c5d2
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-01-04 09:36:10 -0600

    Updated the length of password

--- database/users_migration.php
+++ database/users_migration.php
@@ -14,7 +14,7 @@ $createTable = 'CREATE TABLE users (
     email VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    password VARCHAR(20) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (email)
 )';
